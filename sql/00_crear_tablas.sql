310739b88a5bf242d863aaeea449ce3bbb2c08d7
Manuel Ruz <manuel22ene@gmail.com>
2021-10-19 14:19:16 +0200

    1

--- sql/00_crear_tablas.sql
+++ sql/00_crear_tablas.sql
@@ -38,3 +38,48 @@ CREATE TABLE events (
 ALTER TABLE events
     MODIFY id int(11) NOT NULL AUTO_INCREMENT;
 
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-07-2021 a las 21:30:11
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `practicas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `eventoscalendar`
+--
+
+CREATE TABLE eventoscalendar (
+                                   id int(11) NOT NULL,
+                                   evento varchar(250) DEFAULT NULL,
+                                   color_evento varchar(20) DEFAULT NULL,
+                                   fecha_inicio varchar(20) DEFAULT NULL,
+                                   fecha_fin varchar(20) DEFAULT NULL
+);
+
+INSERT INTO eventoscalendar (id, evento, color_evento, fecha_inicio, fecha_fin) VALUES
+
+ALTER TABLE eventoscalendar
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE eventoscalendar
+    MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+COMMIT;
