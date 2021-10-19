42bcd9aad169021e36f935b66d2454852f8d9b14
Manuel Ruz <manuel22ene@gmail.com>
2021-10-19 13:45:52 +0200

    Initial commit

---
+++ webapp/calendario/pruebacalendar.php
@@ -0,0 +1,28 @@
+<?php
+//index.php
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jquery Fullcalandar Integration with PHP and Mysql</title>
+    <link rel="stylesheet" href="archivosCalendar/fullcalendar.css" />
+    <link rel="stylesheet" href="archivosCalendar/bootstrap.css" />
+    <script src="archivosCalendar/jquery-ui.min.js"></script>
+    <script src="archivosCalendar/jquery.min.js"></script>
+    <script src="archivosCalendar/moment.min.js"></script>
+    <script src="archivosCalendar/fullCalendar.min.js"></script>
+    <script src="calendar.js"></script>
+</head>
+<body>
+<br/>
+<h2 align="center"><a href="#">Jquery Fullcalandar Integration with PHP and Mysql</a></h2>
+<br/>
+<div class="container">
+    <div id="calendar"></div>
+</div>
+</body>
+</html>
