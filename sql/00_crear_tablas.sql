71e9d0dc589cfce2f0ce7d393e286439419f5a8b
Manuel Ruz <manuel22ene@gmail.com>
2021-10-21 15:05:18 +0200

    2

--- sql/00_crear_tablas.sql
+++ sql/00_crear_tablas.sql
@@ -1,84 +1,53 @@
 USE quevedodb;
 
--- Crear tabla usuarios
 CREATE TABLE usuarios (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    passwd VARCHAR(255) NOT NULL,
-    fecha_alta DATETIME DEFAULT CURRENT_TIMESTAMP
+                          id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                          email VARCHAR(255) NOT NULL UNIQUE,
+                          passwd VARCHAR(255) NOT NULL,
+                          fecha_alta DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Crear tabla vacuna
 CREATE TABLE vacuna (
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(20) NOT NULL,
-    nombre_largo VARCHAR(100) NOT NULL,
-    fabricante VARCHAR(255) NOT NULL,
-    num_dosis INT(10) NOT NULL,
-    tiempo_minimo INT,
-    tiempo_maximo INT
+                        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                        nombre VARCHAR(20) NOT NULL,
+                        nombre_largo VARCHAR(100) NOT NULL,
+                        fabricante VARCHAR(255) NOT NULL,
+                        num_dosis INT(10) NOT NULL,
+                        tiempo_minimo INT,
+                        tiempo_maximo INT
 );
---
--- Database: `testing`
---
 
--- --------------------------------------------------------
 
---
--- Table structure for table `events`
---
 
-CREATE TABLE events (
-    id int(11) NOT NULL PRIMARY KEY,
-    title varchar(255) NOT NULL,
-    start_event datetime NOT NULL,
-    end_event datetime NOT NULL
-    ); ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE pacientes (
+                           id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                           nombre VARCHAR(20) NOT NULL,
+                           apellidos VARCHAR(100) NOT NULL,
+                           DNI VARCHAR(9) NOT NULL,
+                           edad VARCHAR(3) NOT NULL
 
-ALTER TABLE events
-    MODIFY id int(11) NOT NULL AUTO_INCREMENT;
-
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 14-07-2021 a las 21:30:11
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.4
+);
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `practicas`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `eventoscalendar`
---
-
 CREATE TABLE eventoscalendar (
-                                   id int(11) NOT NULL,
-                                   evento varchar(250) DEFAULT NULL,
-                                   color_evento varchar(20) DEFAULT NULL,
-                                   fecha_inicio varchar(20) DEFAULT NULL,
-                                   fecha_fin varchar(20) DEFAULT NULL
+                        id int(11) NOT NULL,
+                        evento varchar(250) DEFAULT NULL,
+                        color_evento varchar(20) DEFAULT NULL,
+                        fecha_inicio varchar(20) DEFAULT NULL,
+                        fecha_fin varchar(20) DEFAULT NULL
 );
 
-INSERT INTO eventoscalendar (id, evento, color_evento, fecha_inicio, fecha_fin) VALUES
+INSERT INTO eventoscalendar (id, evento, color_evento, fecha_inicio, fecha_fin) VALUES (51, 'Mi Primera Prueba', 'teal', '2021-07-07', '2021-07-08'),
+                                                                                       (52, 'Mi Segunda Prueba', 'amber', '2021-07-17', '2021-07-18'),
+                                                                                       (53, 'Mi Tercera Prueba', 'orange', '2021-07-03', '2021-07-04');
+
 
 ALTER TABLE eventoscalendar
-    ADD PRIMARY KEY (id);
+    ADD PRIMARY KEY (`id`);
 
 ALTER TABLE eventoscalendar
     MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
---
+++ webapp/calendario/index.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Mi Calendario:: Ing. Urian Viera</title>
+	<link rel="stylesheet" href="">
+	<link rel="stylesheet" type="text/css" href="css/fullcalendar.min.css">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/home.css">
+</head>
+<body>
+
+<?php
+include('config.php');
+
+  $SqlEventos   = ("SELECT * FROM eventoscalendar");
+  $resulEventos = mysqli_query($con, $SqlEventos);
+
+?>
+
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a class="navbar-brand" href="imagenes/logo%20empresa.png">Carousel</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" href="">Crear Vacuna</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link active" href="">lista de vacunas</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link active" href="pacientes.php">Registrar pacientes</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown07" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Opciones</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown07">
+                        <a class="dropdown-item" href="#">Perfil</a>
+                        <a class="dropdown-item" href="logout.php">cerrar Sesion</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+
+<div class="mt-5"></div>
+
+<div class="container">
+  <div class="row">
+    <div class="col msjs">
+      <?php
+        include('msjs.php');
+      ?>
+    </div>
+  </div>
+
+<div class="row">
+  <div class="col-md-12 mb-3">
+  <h3 class="text-center" id="title">Calendario de citas</h3>
+  </div>
+</div>
+</div>
+
+
+
+<div id="calendar"></div>
+
+
+<?php  
+  include('modalNuevoEvento.php');
+  include('modalUpdateEvento.php');
+?>
+
+
+
+<script src ="js/jquery-3.0.0.min.js"> </script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<script type="text/javascript" src="js/moment.min.js"></script>	
+<script type="text/javascript" src="js/fullcalendar.min.js"></script>
+<script src='locales/es.js'></script>
+
+<script type="text/javascript">
+$(document).ready(function() {
+  $("#calendar").fullCalendar({
+    header: {
+      left: "prev,next today",
+      center: "title",
+      right: "month,agendaWeek,agendaDay"
+    },
+
+    locale: 'es',
+
+    defaultView: "month",
+    navLinks: true, 
+    editable: true,
+    eventLimit: true, 
+    selectable: true,
+    selectHelper: false,
+
+//Nuevo Evento
+  select: function(start, end){
+      $("#exampleModal").modal();
+      $("input[name=fecha_inicio]").val(start.format('DD-MM-YYYY'));
+       
+      var valorFechaFin = end.format("DD-MM-YYYY");
+      var F_final = moment(valorFechaFin, "DD-MM-YYYY").subtract(1, 'days').format('DD-MM-YYYY'); //Le resto 1 dia
+      $('input[name=fecha_fin').val(F_final);  
+
+    },
+      
+    events: [
+      <?php
+       while($dataEvento = mysqli_fetch_array($resulEventos)){ ?>
+          {
+          _id: '<?php echo $dataEvento['id']; ?>',
+          title: '<?php echo $dataEvento['evento']; ?>',
+          start: '<?php echo $dataEvento['fecha_inicio']; ?>',
+          end:   '<?php echo $dataEvento['fecha_fin']; ?>',
+          color: '<?php echo $dataEvento['color_evento']; ?>'
+          },
+        <?php } ?>
+    ],
+
+
+//Eliminar Evento
+eventRender: function(event, element) {
+    element
+      .find(".fc-content")
+      .prepend("<span id='btnCerrar'; class='closeon material-icons'>&#xe5cd;</span>");
+    
+    //Eliminar evento
+    element.find(".closeon").on("click", function() {
+
+  var pregunta = confirm("Deseas Borrar este Evento?");   
+  if (pregunta) {
+
+    $("#calendar").fullCalendar("removeEvents", event._id);
+
+     $.ajax({
+            type: "POST",
+            url: 'deleteEvento.php',
+            data: {id:event._id},
+            success: function(datos)
+            {
+              $(".alert-danger").show();
+
+              setTimeout(function () {
+                $(".alert-danger").slideUp(500);
+              }, 3000); 
+
+            }
+        });
+      }
+    });
+  },
+
+
+//Moviendo Evento Drag - Drop
+eventDrop: function (event, delta) {
+  var idEvento = event._id;
+  var start = (event.start.format('DD-MM-YYYY'));
+  var end = (event.end.format("DD-MM-YYYY"));
+
+    $.ajax({
+        url: 'drag_drop_evento.php',
+        data: 'start=' + start + '&end=' + end + '&idEvento=' + idEvento,
+        type: "POST",
+        success: function (response) {
+         // $("#respuesta").html(response);
+        }
+    });
+},
+
+//Modificar Evento del Calendario 
+eventClick:function(event){
+    var idEvento = event._id;
+    $('input[name=idEvento').val(idEvento);
+    $('input[name=evento').val(event.title);
+    $('input[name=fecha_inicio').val(event.start.format('DD-MM-YYYY'));
+    $('input[name=fecha_fin').val(event.end.format("DD-MM-YYYY"));
+
+    $("#modalUpdateEvento").modal();
+  },
+
+
+  });
+
+
+//Oculta mensajes de Notificacion
+  setTimeout(function () {
+    $(".alert").slideUp(300);
+  }, 3000); 
+
+
+});
+
+</script>
+</body>
+</html>
